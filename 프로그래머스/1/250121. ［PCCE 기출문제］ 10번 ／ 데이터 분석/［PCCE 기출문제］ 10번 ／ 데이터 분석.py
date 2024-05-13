68fb51e67a7f2921eaec9f22405ed765044d58bf
penguin <56948757+penguin-learning@users.noreply.github.com>
2024-05-14 07:44:23 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,17 @@
+def solution(data, ext, val_ext, sort_by):
+    answer = []
+    
+    index_dict = {
+                  "code": 0,
+                  "date": 1,
+                  "maximum": 2,
+                  "remain": 3
+                 }
+    
+    for row in data:
+        if row[index_dict[ext]] < val_ext:
+            answer.append(row)
+    
+    answer.sort(key = lambda x : x[index_dict[sort_by]])
+    
+    return answer 
