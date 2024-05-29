b1f57b44e067599d6192cd19688d9e722fed923f
penguin <56948757+penguin-learning@users.noreply.github.com>
2024-05-29 19:39:00 +0900

    [level 1] Title: 최소직사각형, Time: 3.95 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,13 @@
+def solution(sizes):
+    answer = 0
+    
+    w = []
+    h = []
+    
+    for s in sizes:
+        w.append(max(s))
+        h.append(min(s))
+    
+    answer = max(w) * max(h)
+    
+    return answer
